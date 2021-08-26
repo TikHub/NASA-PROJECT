52b4806039673e832eb96d77fa6f0f9e7d3b79cd
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2021-08-26 15:33:55 +0400

    SERVER: GET /planets implementation, corresponding request in POSTMAN has been created. Model, Router and Controller have been created for planets.

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 
+const planetsRouter = require("./routes/planets/planets.router");
+
 const app = express(); // Setup Express application
+
+app.use(express.json());
+
+app.use(planetsRouter);
 
 module.exports = app;
--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,10 @@
 const http = require("http");
 const app = require("./app");
 
-const server = http.createServer(app);
-
 // Use environment variable PORT (PORT=XXXX in packages.json -> scripts -> [SCRIPT]) or 8000
 const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
 
 // listen function for Express is exactly the same as listen function for server (http.createServer())
 // So it is possible to separate Express functionality (app.js) from server (server.js) in separate files
