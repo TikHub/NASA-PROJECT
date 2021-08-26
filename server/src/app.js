3ad770fcf72628fb7e29e921cc1544e460c71ccc
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2021-08-26 22:35:01 +0400

    PROJECT: Automation with common package.json in root directory. Updated .gitignore file for server. Technical debt: implement whitelist in case of running both client and server with one command.

--- server/src/app.js
+++ server/src/app.js
@@ -5,17 +5,18 @@ const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express(); // Setup Express application
 
-const whitelist = ["http://localhost:3000"];
+// TODO Implement Whitelist, CORS error in case of running "npm run watch" in common package.json
+// const whitelist = ["http://localhost:3000"];
 app.use(
   cors({
-    // origin: "http://localhost:3000", // Allow CORS from this URL (Origin)
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: "http://localhost:3000", // Allow CORS from this URL (Origin)
+    // origin: function (origin, callback) {
+    //   if (whitelist.indexOf(origin) !== -1) {
+    //     callback(null, true);
+    //   } else {
+    //     callback(new Error("Not allowed by CORS"));
+    //   }
+    // },
   })
 );
 app.use(express.json());
