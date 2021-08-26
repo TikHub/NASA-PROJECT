d011ed464f57c1126d3837390314e6a2773c90cf
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2021-08-26 18:32:24 +0400

    SERVER: CORS package has been added. Whitelist.

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const cors = require("cors");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express(); // Setup Express application
 
+const whitelist = ["http://localhost:3000"];
+app.use(
+  cors({
+    // origin: "http://localhost:3000", // Allow CORS from this URL (Origin)
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 app.use(express.json());
 
 app.use(planetsRouter);
