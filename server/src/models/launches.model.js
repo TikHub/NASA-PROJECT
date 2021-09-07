57b4d7031637bfc59da20effaeeea85cc5272eeb
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2021-09-07 21:00:02 +0400

    SERVER: Launch model. POST launch Postman implementation.

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -3,6 +3,8 @@ const path = require("path");
 
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: "Kepler Exploration X",
@@ -20,6 +22,20 @@ function getAllLaunches() {
   return Array.from(launches.values()); // Turns the iterable 'launches.values()' into an array, containing all of that values
 }
 
+function addNewLaunch(launch) {
+  latestFlightNumber += 1;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      flightNumber: latestFlightNumber,
+      customer: ["Zero to Mastery", "NASA"],
+      upcoming: true,
+      success: true,
+    })
+  );
+}
+
 module.exports = {
   getAllLaunches,
+  addNewLaunch,
 };
