e630d2d022942e0fcfbe7112257a182e6b87b2d4
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2021-09-09 10:38:29 +0400

    SERVER: First test, to check GET /launches (status, headers)

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,7 +1,12 @@
+const request = require("supertest");
+const app = require("../../app");
+
 describe("Test GET /launches", () => {
-  test("It should respond with 200 success", () => {
-    response = 200;
-    expect(response).toBe(200);
+  test("It should respond with 200 success and 'Content-Type' to include the word 'json'", async () => {
+    response = await request(app)
+      .get("/launches")
+      .expect("Content-Type", /json/)
+      .expect(200);
   });
 });
 
