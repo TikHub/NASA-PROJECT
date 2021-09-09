67d4058ddceb1c3d1715d6b0b8fef9d5ca5e91ad
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2021-09-09 10:23:52 +0400

    SERVER: Installed Jest and SuperTest npm packages in server folder. Change test script and added test-watch script. Updated test scripts in project by removing "run" command. Test structure for launches.

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,13 @@
+describe("Test GET /launches", () => {
+  test("It should respond with 200 success", () => {
+    response = 200;
+    expect(response).toBe(200);
+  });
+});
+
+describe("Test POST /launches", () => {
+  test("It should respond with 200 success", () => {});
+
+  test("It should catch required properties", () => {});
+  test("It should catch invalid dates", () => {});
+});
